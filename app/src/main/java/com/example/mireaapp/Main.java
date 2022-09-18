32346a4f857b381bc22b95d64b5b553ffd349b08
Nikita <nikita2003.ns@gmail.com>
2022-09-18 23:18:14 +0300

    FIX: Reformat programm

--- app/src/main/java/com/example/mireaapp/Main.java
+++ app/src/main/java/com/example/mireaapp/Main.java
@@ -1,35 +1,30 @@
 package com.example.mireaapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
 
 public class Main extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.home_screen);
+        setContentView(R.layout.home_screedn);
         Button login = findViewById(R.id.loginButton);
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Log.i("Info", "Log in button tapped");
-                //TODO: Add navigation to Log In screen
-            }
+        login.setOnClickListener(view -> {
+            Log.i("Info", "Log in button tapped");
+            //TODO: Add navigation to Log In screen
         });
 
 
         Button signup = findViewById(R.id.signupButton);
-        signup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Log.i("Info", "Sign up button tapped");
-                //TODO: Add navigation to Sign Up screen
-            }
+        signup.setOnClickListener(view -> {
+            Log.i("Info", "Sign up button tapped");
+            //TODO: Add navigation to Sign Up screen
         });
     }
 }
