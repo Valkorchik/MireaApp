24e2a2e2dd8385269bc1edf85ae3d3ad5b71781e
Nikita <nikita2003.ns@gmail.com>
2022-10-19 21:17:39 +0300

    FEATURE: Add change password functional

--- app/src/main/java/com/example/mireaapp/ChangePassword.java
+++ app/src/main/java/com/example/mireaapp/ChangePassword.java
@@ -1,33 +1,62 @@
 package com.example.mireaapp;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
-import android.os.Bundle;
-import android.widget.Button;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class ChangePassword extends AppCompatActivity {
     Button button;
+    EditText newPassword;
+    EditText newPasswordVerify;
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.change_password_screen);
-        button=findViewById(R.id.changePassword);
+        newPassword = findViewById(R.id.editTextNewPassword);
+        newPasswordVerify = findViewById(R.id.editTextNewPasswordVerify);
+        newPassword.setError("Password should be at least 6 characters");
+        newPasswordVerify.setError("Password should be at least 6 characters");
+        button = findViewById(R.id.changePassword);
         button.setOnClickListener(view -> {
-            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-            String newPassword = "SOME-SECURE-PASSWORD";
-
-            user.updatePassword(newPassword)
-                    .addOnCompleteListener(new OnCompleteListener<Void>() {
-                        @Override
-                        public void onComplete(@NonNull Task<Void> task) {
-                            if (task.isSuccessful()) {
-                                Log.d(TAG, "User password updated.");
+            if (validatePassword(newPassword, newPasswordVerify)) {
+                user.updatePassword(newPassword.toString())
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Log.d("User", "User password updated.");
+                                }
                             }
-                        }
-                    });
-            finish();
+                        });
+                finish();
+            }
+
+
         });
 
+    }
+
+    public boolean validatePassword(EditText newPassword, EditText newPasswordVerify) {
+        if (newPassword.toString().length() < 6 | newPasswordVerify.toString().length() < 6) {
+            Toast.makeText(this, "Passwords should be at least 6 characters", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        if (newPassword != newPasswordVerify) {
+            Toast.makeText(this, "Passwords are not equal", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
     }
 }
